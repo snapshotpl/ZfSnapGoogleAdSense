390b001af272bd1f7d9a57175fa0d4613350ab6b
snapshotpl <witold@wasiczko.pl>
2014-02-24 00:28:19 +0100

    Move module class to src

--- Module.php
+++ Module.php
@@ -1,37 +1,3 @@
 <?php
 
-namespace ZfSnapGoogleAdsense;
-
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
-
-/**
- * Class Module
- *
- * @package ZfSnapGoogleAdsense
- * @author  Witold Wasiczko <witold@wasiczko.pl>
- */
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface
-{
-    /**
-     * @return array
-     */
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
-    /**
-     * @return array
-     */
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-}
+require __DIR__ . '/src/ZfSnapGoogleAdsense/Module.php';
---
+++ src/ZfSnapGoogleAdsense/Module.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ZfSnapGoogleAdsense;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+/**
+ * Class Module
+ *
+ * @package ZfSnapGoogleAdsense
+ * @author  Witold Wasiczko <witold@wasiczko.pl>
+ */
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+{
+    /**
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/../' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+}
